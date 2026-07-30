e96f0fbffc087fd87210e8f341ca65038f1354ae
Michael Sanchez <20240063@itla.edu.do>
2026-07-30 00:10:50 -1100

    test: agregar pruebas Selenium de HU-03 y ampliar ProductsPage

--- pages/products_page.py
+++ pages/products_page.py
@@ -27,6 +27,7 @@ class ProductsPage:
         self.espera.until(
             lambda driver: driver.find_elements(*self.TABLA)
             or driver.find_elements(*self.LISTADO_VACIO)
+            or "/login" in driver.current_url
         )
         return self
 
@@ -40,11 +41,26 @@ class ProductsPage:
         )
         return elemento.text
 
+    def esperar_tabla_visible(self):
+        self.espera.until(EC.visibility_of_element_located(self.TABLA))
+        return self
+
+    def esperar_listado_vacio(self):
+        self.espera.until(EC.visibility_of_element_located(self.LISTADO_VACIO))
+        return self
+
+    def esta_tabla_visible(self):
+        elementos = self.driver.find_elements(*self.TABLA)
+        return bool(elementos) and elementos[0].is_displayed()
+
     def esta_listado_vacio(self):
         try:
             return self.driver.find_element(*self.LISTADO_VACIO).is_displayed()
         except NoSuchElementException:
             return False
+
+    def contar_filas(self):
+        return len(self.driver.find_elements(*self.FILAS))
 
     def buscar_fila_por_nombre(self, nombre):
         filas = self.driver.find_elements(*self.FILAS)
@@ -57,8 +73,16 @@ class ProductsPage:
     def esperar_fila_por_nombre(self, nombre):
         return self.espera.until(lambda driver: self.buscar_fila_por_nombre(nombre))
 
+    def producto_existe(self, nombre):
+        return self.buscar_fila_por_nombre(nombre) is not None
+
+    def obtener_id_fila(self, fila):
+        id_atributo = fila.get_attribute("id")
+        return int(id_atributo.replace("fila-producto-", ""))
+
     def obtener_datos_fila(self, fila):
         return {
+            "id": self.obtener_id_fila(fila),
             "nombre": fila.find_element(*self.CELDA_NOMBRE).text,
             "precio": fila.find_element(*self.CELDA_PRECIO).text,
             "cantidad": fila.find_element(*self.CELDA_CANTIDAD).text,
